9bd836ef9c9c15f14161ba21e4fc9aa27d589c4f
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2019-01-23 22:16:29 +0700

    Update register client

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/login');
+});
+
+router.get('/register', (req,res)=>{
+  res.render('pages/register',{user: {}, error:{}});
+});
+
+router.post('/register',(req,res)=>{
+  const body = req.body;
+  console.log(body);
+  res.render('pages/register',{user: body, error:{email: 'Exists'}});
 });
 
 module.exports = router;
